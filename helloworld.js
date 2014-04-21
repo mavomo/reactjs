70194cd7e0fea351131cc683bc88b5e0d07a23b9
msherefel <michelle.avomo@gmail.com>
2014-04-21 14:49:15 +0200

    update name

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,17 @@
 /** @jsx React.DOM */
-var App = React.createClass({
+var myFirstComponent = React.createClass({
 	render : function(){
+		//La fonction render, ne peut retourner qu'un element. 
+
 		return (
-			<h1> Hello world. I am a component! </h1>
-			)
+				<div>
+					<h1> Hello world. I am a component! </h1>
+					<p> You have nothing to worry about.</p>
+					<p> What is your name? </p>
+				</div>
+			   )
 	}
 });
-
-React.renderComponent(<App />, example);
+//Appel du composant. 
+// Le deuxième paramètre doit correspondre à un tag du fichier HTML Défini.
+React.renderComponent(<myFirstComponent />, helloooo);
